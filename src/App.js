97ae3b6d65e9109f0d812076e2030a8c11367171
Jaylen Schelb <jschelb@thinksurgical.com>
2023-07-14 15:52:15 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
---
+++ src/App.test.js
@@ -0,0 +1,43 @@
+import ReactThreeTestRenderer from "@react-three/test-renderer";
+import { Box, Text } from "@react-three/drei";
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
+
+test("example test 1", async () => {
+  window.HTMLCanvasElement.prototype.getContext = () => {};
+  const renderer = await ReactThreeTestRenderer.create(
+    <group>
+      <Box name="box_test" />
+    </group>
+  );
+
+  console.log(renderer.scene.instance.children[0].children);
+
+  // Dummy Expect
+  expect(renderer).toBeTruthy();
+});
+
+test("example test 2", async () => {
+  const renderer = await ReactThreeTestRenderer.create(
+    <group>
+      <Box name="box_test" />
+      <Text name="text_test"> Testing </Text>
+    </group>
+  );
+
+  console.log(renderer.scene.instance.children);
+
+  // Dummy Expect
+  expect(renderer).toBeTruthy();
+});
+
+test("example test 3", async () => {
+  const renderer = await ReactThreeTestRenderer.create(
+    <Text name="text_test"> Testing </Text>
+  );
+
+  console.log(renderer.scene.instance.children);
+
+  // Dummy Expect
+  expect(renderer).toBeTruthy();
+});
